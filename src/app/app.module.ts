81e6763ae7b3cda819c7d6f347dc8f9aa256fbcd
Sagnella Franco <Fransagn@gmail.com>
2021-10-06 20:16:20 -0300

    Creado repo vacio para usar maniana

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,27 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import {AngularFireModule} from '@angular/fire/compat';
+import {AngularFireAuthModule} from '@angular/fire/compat/auth';
+import {AngularFirestoreModule} from '@angular/fire/compat/firestore';
+import { environment } from 'src/environments/environment';
+import { ReactiveFormsModule } from '@angular/forms';
+import {HttpClient, HttpClientModule, HttpHandler} from '@angular/common/http';
+import {AngularFireStorageModule} from '@angular/fire/compat/storage';
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AngularFireModule.initializeApp(environment.firebaseConfig),
+    AngularFireAuthModule,
+    AngularFirestoreModule,
+    HttpClientModule,
+    ReactiveFormsModule,
+    AngularFireStorageModule
   ],
   providers: [],
   bootstrap: [AppComponent]
